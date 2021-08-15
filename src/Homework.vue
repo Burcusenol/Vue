ab8c957a345b29cb6dfe08c0b5f73db5515f6a0b
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-08-16 00:17:46 +0300

    16.08.2021

---
+++ src/Homework.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <!-- 1) Interpolation yani {{}} Syntax'ını kullanarak <p> elementinin içeriğini Name ve Age verileriyle doldurun-->
+    <p>VueJS Muazzam bir Framework - {{ name }} ({{ age }})</p>
+    <!-- 2) Yaşınızı 5 ile çarpıp <p> içerisinde yazdırınız -->
+
+    <p>{{age*5}}</p>
+
+    <!-- 3) Fonksiyon çağırarak 0 ile 1 arasında bir sayı üretip <p> içerisinde yazdırınız (Math.random()) -->
+    <p>{{random()}}</p>
+
+    <!-- 4) Google üzerinden bulacağınız herhangi bir fotoğrafı <img> elementinde gösterme işlemini yapınızı. Bunu yaparken "src" attribute' una bind etmeyi unutmayınız -->
+    <img v-bind:src="img"/>
+    <!-- 5) Name değişkeninin değerinin <input /> içine yazdığınız değer ile anlık olarak değiştirilmesini sağlayınız. ("value" attribute kullanarak) -->
+    <div>
+      <input type="text" v-on:input="changeTitle" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function() {
+    return {
+      name:"Burcu",
+      age:23,
+      img:"https://lh3.googleusercontent.com/proxy/PhE30DX4XPv_G1Pe1g0iI2daj2_7yOkg2FbmAwnuqUa--JCy0hN76qHHyhyjxOe4-zmDcQ-dOwwlsAGEUzIbbUSWM8MyTJJoeWdho2vuMWak",
+    };
+  },
+  methods: {
+    changeTitle: function(event) {
+      this.name = event.target.value;
+    },
+    random:function(){
+      var sayi=Math.random();
+      return sayi;
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/Homework2.vue
@@ -0,0 +1,48 @@
+<template>
+ <div id="exercise">
+    <!-- 1) Butona tıklanıldığında bir alert kutusu çıkartınız -->
+    <div>
+        <button v-on:click="showAlert">Alert Göster</button>
+    </div>
+    <!-- 2) "keydown" event'ini dinleyerek value değişkeni içerisinde yazdığınız veriyi saklayın. (ipucu: event.target.value size yazdığınız değeri verebilir.) -->
+    <div>
+        <input type="text" v-on:keydown="show">
+        <p>{{ value }}</p>
+    </div>
+    <!-- 3) 2.Uygulamayı düzenleyerek eğer keydown ENTER ise yazdığınız verinin value değerine aktarılmasını sağlayın -->
+    <div>
+        <input type="text" v-on:keydown.enter="show">
+        <p>{{ value }}</p>
+    </div>
+</div>
+ 
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  
+  methods: {
+    
+    showAlert:function(){
+      alert('Alert');
+    },
+    show:function(event){
+      alert(event.target.value)
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
